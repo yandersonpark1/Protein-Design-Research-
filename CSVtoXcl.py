f3c26e6b14ddba7dbae58a42daaa45fa376cb741
yandersonpark1 <everydayap3@gmail.com>
2025-06-26 16:37:09 -0400

    final edits

--- CSVtoXcl.py
+++ CSVtoXcl.py
@@ -16,20 +16,14 @@ class CSVtoXcl:
         self.df.to_excel(self.output_file, index=False)
         print(f"Converted {self.input_file} to {self.output_file}")
 
-def convert_csvs_in_folder(input_file): #folder_path = 'Tyr_ClpS_250624/V7'
-    # List all files in the folder
-    # for filename in os.listdir(folder_path):
-    #     if filename == input_file:
-    #         input_path = os.path.join(folder_path, filename)
-    #         base_name = os.path.splitext(filename)[0]
-    #         output_path = os.path.join(folder_path, base_name + '.xlsx')
+def convert_csvs(input_file):
     output_path = (input_file.removesuffix('.csv') + '.xlsx')
     converter = CSVtoXcl(input_file, output_path)
     converter.convert()
          
 def main(): 
     input_file = input("Enter the name of the CSV file to convert (e.g., 'data.csv'): ")
-    convert_csvs_in_folder(input_file)
+    convert_csvs(input_file)
     
 if __name__ == "__main__":
     main()
