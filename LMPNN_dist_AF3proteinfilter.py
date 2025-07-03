a97be0b3bcb45ba7066ca4ae47ef83037833962c
yandersonpark1 <everydayap3@gmail.com>
2025-07-03 16:53:27 -0400

    v7 pipeline and edits to normall mpnn and af3 pipeline

--- LMPNN_dist_AF3proteinfilter.py
+++ LMPNN_dist_AF3proteinfilter.py
@@ -12,7 +12,7 @@ class LMPNN_dist_Af3proteinfilter():
         self.df = pd.read_excel(input_file, header=0, usecols=['diff', 'bb', 'seq #', 'plDDT', 'plDDT A', 'plDDT B', 'pTM', 'ipTM', 'evo pro', 'ctct score', '# ctct', 'PAE/ ctct', '# ctct@residue', 'PAE/ ctct@residue','ctct score@residue', 'hbond count', 'hbond b1', 'hbond b2', 'hbond b3', 'B1 total SASA', 'b1 bb', 'b1 sc', 'B2 total SASA', 'b2 bb', 'b2 sc', 'B3 total SASA', 'b3 bb', 'b3 sc'])
         self.filtered_df = self.df.copy()
     
-    def plDDT(self, min_plDDT = 89.5, column = "plDDT A"): 
+    def plDDT(self, min_plDDT = 80, column = "plDDT A"): 
         """
         plDDT is a measure of the local distance difference test, which is a metric for the quality of protein structure predictions.
         > 90 is considered high confidence with confidence in backbone and side chains, 
--- combinehbond_SASA_AF3.py
+++ combinehbond_SASA_AF3.py
@@ -9,11 +9,11 @@ class combinehbond_SASA_AF3:
         ''' Change depending on the input file format '''
         self.hbond_df = pd.read_excel(hbond_file, header = 0, usecols=['hbond count', 'hbond b1', 'hbond b2', 'hbond b3'])
         self.SASA_file = pd.read_excel(SASA_file, header = 0, usecols=['B1 total SASA', 'b1 bb', 'b1 sc', 'B2 total SASA', 'b2 bb', 'b2 sc', 'B3 total SASA', 'b3 bb', 'b3 sc'])
-        self.AF3_file = pd.read_excel(AF3_file, header = 0, usecols=['diff', 'bb', 'seq #', 'plDDT', 'plDDT A', 'plDDT B', 'pTM', 'ipTM', 'evo pro', 'ctct score', '# ctct', 'PAE/ ctct', '# ctct@residue', 'PAE/ ctct@residue','ctct score@residue'])
+        self.AF3_file = pd.read_excel(AF3_file, header = 0, usecols=['diff', 'bb', 'seq #', 'seq name', 'plDDT', 'plDDT A', 'plDDT B', 'pTM', 'ipTM', 'evo pro', 'ctct score', '# ctct', 'PAE/ ctct'])
     
     def combine(self, version): 
         combined = pd.concat([self.AF3_file, self.hbond_df, self.SASA_file], axis=1)
-        combined.to_excel(f'{version}_dist_metrics.xlsx', index=False)
+        combined.to_excel(f'{version}_metrics.xlsx', index=False)
     
 
 def main():
